c80fa63189854440d2afd8cdef827c0347c679ff
Sai Kumar Palreddy <76553984+palreddysai@users.noreply.github.com>
2023-10-25 11:52:09 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './authentication/sign-up/sign-up.component';
+import { LogInComponent } from './authentication/log-in/log-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    LogInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
